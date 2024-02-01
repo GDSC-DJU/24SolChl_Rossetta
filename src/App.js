8d299859ca3b3c0e3c0c7b76b0ccb9a072381765
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-02-01 20:11:03 +0900

    feat: Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Login from "./components/Login";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
 import Nav from "./components/Nav";
+// import Main from "./components/Main";
 import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav />
-    </div>
+    <>
+		<Nav />
+      	<Routes>
+			<Route path="/" element={<Main/>}/>
+			<Route path="Login" element={<Login/>}/>
+		</Routes>
+    </>
   );
 }
 
 export default App;
+
+
