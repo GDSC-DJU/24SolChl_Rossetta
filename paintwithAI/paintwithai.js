17442a38cd487213477134d1602b4a28c4caf11d
EOTAEGYU <wkdrnxorb@naver.com>
2024-02-02 16:42:12 +0900

    feat:손가락 접힘 유무 함수 추가 및 검지 손가락만 올라와 있을 때 그림 그리기

--- paintwithAI/paintwithai.js
+++ paintwithAI/paintwithai.js
@@ -1,6 +1,7 @@
 const videoElement = document.getElementById('video');
 const canvasElement = document.getElementById('canvas');
 const canvasCtx = canvasElement.getContext('2d');
+
 let xp = 0, yp = 0;
 let drawColor = 'rgb(255, 0, 0)'; // 빨간색
 let thickness = 20;
@@ -33,18 +34,46 @@ const camera = new Camera(videoElement, {
 
 camera.start();
 
+function areFingersUp(landmarks) {
+    const tipIds = [4, 8, 12, 16, 20];
+    let fingersUp = [0, 0, 0, 0, 0];
+
+    // 엄지 손가락은 x 좌표를 비교하여 판단
+    if (landmarks[tipIds[0]].x > landmarks[tipIds[0] - 1].x) {
+        fingersUp[0] = 1; // 엄지손가락이 올라와 있음
+    }
+
+    // 나머지 손가락들은 y 좌표를 비교하여 판단
+    for (let i = 1; i < tipIds.length; i++) {
+        if (landmarks[tipIds[i]].y < landmarks[tipIds[i] - 2].y) {
+            fingersUp[i] = 1; // 해당 손가락이 올라와 있음
+        }
+    }
+
+    return fingersUp;
+}
+
+
 function onResults(results) {
     if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
         const landmarks = results.multiHandLandmarks[0];
-        const indexFingerTipX = canvasElement.width - (landmarks[8].x * canvasElement.width);
-        const indexFingerTipY = landmarks[8].y * canvasElement.height;
+        const fingersUp = areFingersUp(landmarks);
 
-        if (xp !== 0 && yp !== 0) {
-            drawLine(xp, yp, indexFingerTipX, indexFingerTipY);
+        // 검지손가락만 올라가 있고, 나머지 손가락들은 접혀있는지 확인
+        if (fingersUp[1] === 1 && fingersUp.every((val, i) => i === 1 || val === 0)) {
+            const indexFingerTipX = canvasElement.width - (landmarks[8].x * canvasElement.width);
+            const indexFingerTipY = landmarks[8].y * canvasElement.height;
+
+            if (xp !== 0 && yp !== 0) {
+                drawLine(xp, yp, indexFingerTipX, indexFingerTipY);
+            }
+
+            xp = indexFingerTipX;
+            yp = indexFingerTipY;
+        } else {
+            xp = 0;
+            yp = 0;
         }
-
-        xp = indexFingerTipX;
-        yp = indexFingerTipY;
     } else {
         xp = 0;
         yp = 0;
