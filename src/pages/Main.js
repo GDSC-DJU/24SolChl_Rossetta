02407ffa370cd03c4ef2087e598fff431e908c3e
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-02-05 02:20:59 +0900

    feat: Banner check-point

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,10 @@
 import React from 'react'
+import Banner from '../components/Banner'
 
 const Main = () => {
   return (
     <div className='main'>
-      <h1>
-
-      메인페이지입니다
-      </h1>
+        {/* <Banner/> */}
     </div>
   )
 }
