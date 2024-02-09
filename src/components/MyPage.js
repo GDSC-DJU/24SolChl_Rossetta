43f22ac92e1f5d787e967649bb55f8dea790659a
minseok <alsthr0338@gmail.com>
2024-02-09 14:52:26 +0900

    feat: wchsler graph update

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -15,20 +15,54 @@ import {
 import { Line,Radar,Doughnut } from 'react-chartjs-2';
 import '../styles/myPage.css'
 // import { debounce } from 'lodash'
+import img from '../img/images.jpeg'
 
 
 
 const MyPage = () =>{
   ChartJS.register(CategoryScale,RadialLinearScale, LinearScale, PointElement, LineElement,ArcElement,Filler, Title, Tooltip, Legend);
 
-  //웩슬러 지능검사 그래프
-  const [wchslerData,setWechslerData] = useState({
+  //웩슬러 언어 그래프
+  const [wchslerData1,setWechslerData1] = useState({
     datasets: [
       {
         // label: '# of Votes',
-        data: [60,100],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
+        data: [100,60],
+        backgroundColor: ['rgba(	173, 181, 189, 0.2)','rgba(	128, 0, 128, 0.2)'],
+        borderColor: ['rgba(	173, 181, 189, 1)','rgba(	128, 0, 128, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  })
+  const [wchslerData2,setWechslerData2] = useState({
+    datasets: [
+      {
+        // label: '# of Votes',
+        data: [40,120],
+        backgroundColor: ['rgba(	173, 181, 189, 0.2)','rgba(0, 128, 47, 0.2)'],
+        borderColor: ['rgba(	173, 181, 189, 1)','rgba(0, 128, 47, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  })
+  const [wchslerData3,setWechslerData3] = useState({
+    datasets: [
+      {
+        // label: '# of Votes',
+        data: [80,80],
+        backgroundColor: ['rgba(	173, 181, 189, 0.2)','rgba(15, 0, 128, 0.2)'],
+        borderColor: ['rgba(	173, 181, 189, 1)','rgba(15, 0, 128, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  })
+  const [wchslerData4,setWechslerData4] = useState({
+    datasets: [
+      {
+        // label: '# of Votes',
+        data: [70,90],
+        backgroundColor: ['rgba(	173, 181, 189, 0.2)','rgba(246, 1, 1, 0.26)'],
+        borderColor: ['rgba(	173, 181, 189, 1)','rgba(246, 1, 1, 1)'],
         borderWidth: 1,
       },
     ],
@@ -39,40 +73,13 @@ const MyPage = () =>{
     cutout: '78%',
     plugins: {
       legend: {
-        position: 'top',
+        display:false,
       },
       title: {
         display: false,
       },
       
     },
-    // scales: {
-    //   r: {
-    //     // min: 0,
-    //     // max: 100,
-    //     ticks: {
-
-    //       // stepSize: 2.5,
-    //       // display: false,
-    //     },
-    //     grid: {
-    //       // color: ,
-    //     },
-    //     //라벨 속성 지정.
-    //     pointLabels: {
-    //       font: {
-    //         // size: 12,
-    //         // weight: '700',
-    //         // family: 'Pretendard',
-    //       },
-    //       // color: COLOR.BLACK,
-    //     },
-    //     angleLines: {
-    //       display: false,
-    //     },
-        
-    //   },
-    // },
   };
 
   //시험 점수 그래프
@@ -116,30 +123,42 @@ const MyPage = () =>{
           <div className="wchsler-graph-data">
             <div className="wchsler-graph">
               <div>
-                <Doughnut options={wchslerOptions} data={wchslerData}/>
+                <Doughnut options={wchslerOptions} data={wchslerData1}/>
               </div>
               <div>
-                <Doughnut options={wchslerOptions} data={wchslerData}/>
+                <Doughnut options={wchslerOptions} data={wchslerData2}/>
               </div>
               <div>
-                <Doughnut options={wchslerOptions} data={wchslerData}/>
+                <Doughnut options={wchslerOptions} data={wchslerData3}/>
               </div>
               <div>
-                <Doughnut options={wchslerOptions} data={wchslerData}/>
+                <Doughnut options={wchslerOptions} data={wchslerData4}/>
               </div>
             </div>
             
             <div className="wchsler-data">
-
+              <div className="first-color"></div>
+              <div className="percentage"> 37.5%</div>
+              <div className="name">언어 이해</div>
+              <div className="data">60점</div>
             </div>
             <div className="wchsler-data">
-
+              <div className="second-color"></div>
+              <div className="percentage"> 75%</div>
+              <div className="name">지각 추론 이해</div>
+              <div className="data">120점</div>
             </div>
             <div className="wchsler-data">
-
+              <div className="third-color"></div>
+              <div className="percentage"> 50%</div>
+              <div className="name">작업기억</div>
+              <div className="data">80점</div>
             </div>
             <div className="wchsler-data">
-
+              <div className="fourth-color"></div>
+              <div className="percentage"> 56.25%</div>
+              <div className="name">처리속도</div>
+              <div className="data">90점</div>
             </div>
             
             {/* // <Radar ref={chartRef2} options={wchslerOptions} data={wchslerData}/> */}
@@ -170,7 +189,7 @@ const MyPage = () =>{
               </div>
             </div>
             <div className="list-item-continer">
-              <a href="" className="list-columns">
+              <div href="" className="list-columns">
                 <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
@@ -186,10 +205,10 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a>  
+              </div>  
 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -204,10 +223,10 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a>  
+              </div>  
 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -222,10 +241,10 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a>  
+              </div>  
 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -240,10 +259,10 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a>  
+              </div>  
 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -258,9 +277,9 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a>  
-              <a className="list-columns">
-              <div className="list-columns-item">
+              </div>  
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -275,9 +294,9 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a> 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              </div> 
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -292,9 +311,9 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a> 
-              <a className="list-columns">
-              <div className="list-columns-item">
+              </div> 
+              <div className="list-columns">
+                <div className="list-columns-item">
                   반복 문장 말하기
                 </div>
                 <div className="list-columns-item">
@@ -309,7 +328,7 @@ const MyPage = () =>{
                 <div className="list-columns-item">
                   2024-02-03
                 </div>
-              </a> 
+              </div> 
             </div>
           </div>
           
@@ -318,20 +337,26 @@ const MyPage = () =>{
       </div>
       <div className="child-info-container">
         <div className="child-info">
-          <div className="child-info-item">
-            사진
+          <div className="child-info-ficture">
+            <img src={img}/>
           </div>
-          <div className="child-info-item">
-            이름
+          <div className="child-info-item-container">
+            <div className="child-info-item">
+              이름
+            </div>
+            <div className="child-info-item">
+              나이
+            </div>
           </div>
-          <div className="child-info-item">
-            나이
-          </div>
+
         </div>
         <div className="menu-container">
           <a>회원 정보 수정</a>
-          <a>알림함</a>
+          <a>내가 그린 그림들</a>
           <a>공지사항</a>
+        </div>
+        <div className="sign-out">
+          로그아웃
         </div>
       </div>
       
