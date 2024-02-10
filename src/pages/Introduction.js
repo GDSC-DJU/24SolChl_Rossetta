bb39e9b716dd5b4d7e6f516f9f0a0761ed83524f
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-02-11 00:05:01 +0900

    feat: Add Introduction js file

---
+++ src/pages/Introduction.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Introduction = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Introduction
+
+const IntroductionContainer = styled.div`
+    position: relative;
+    display: block;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 70px;
+    font-family: 'Korean-Font-bold';
+    z-index: 1;
+`;
