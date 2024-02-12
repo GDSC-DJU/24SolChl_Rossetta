11e16434f7e26112722ea5c8aab36d1edcdc58c4
홍교 <111673196+LeeHongyo@users.noreply.github.com>
2024-02-13 02:00:20 +0900

    feat:add pattern components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Main from "./pages/Main";
 import Nav from "./components/Nav";
 // import Main from "./components/Main";
 import "./App.css";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet, Routes, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Situation from "./components/Situation";
 import Pronunciation from "./components/Pronunciation";
 import Signup from "./pages/Signup";
+import Pattern1 from "./patterns/pattern";
+import Pattern2 from "./patterns/pattern";
+import Pattern3 from "./patterns/pattern";
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
 			<Route path="login" element={<Login/>}/>
 			<Route path="signup" element={<Signup/>}/>
       		<Route path="pronunciation" element={<Pronunciation/>}/>
+			<Route path="pattern1" element={<Pattern1/>}/>
+			<Route path="pattern2" element={<Pattern2/>}/>
+			<Route path="pattern3" element={<Pattern3/>}/>
 		</Routes>
     </>
 
