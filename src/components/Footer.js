fb0ac5e04bafd14e6be2ad9529748524b00d98bb
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-02-16 14:28:52 +0900

    feat: footer 컴포넌트 생성

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
+
+const FooterContainer = styled.div`
+    position: relative;
+    height: 170px;
+    width: 100%;
+    backgorund-color: #36BC98;
+`;
