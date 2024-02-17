96eb4448ce6de14dbdf90bf6fded197508150fbe
Jinwoo Kim <rlawlsdn9583@gmail.com>
2024-02-17 21:52:39 +0900

    refactor: 배너 체인지

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -6,7 +6,7 @@ const MainBanner = () => {
         <MainBannerWrapper>
             <img 
                 alt="매인배너이미지"
-                src="./assets/main-banner6.png"
+                src="./assets/rossetta-banner.png"
                 style={{top: "100px", width: "100%"}}
             />
         </MainBannerWrapper>
